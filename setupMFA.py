0f446e07c5eb22e92728e5f91521e3f7c6799457
sournachos <joserivera18100@gmail.com>
2024-09-18 18:11:05 -0500

    MFA instructions

--- setupMFA.py
+++ setupMFA.py
@@ -1,9 +1,14 @@
 import pyotp
-def log_in() -> dict | None:
-    # grab the key from Robinhood (Security & Privacy section) and paste it below
-    # save the value below in your .env as MFA_CODE
+# Make changes to this file to get your MFA set up - do NOT commit these changes
+
+def log_in():
+    # grab the key from Robinhood:
+    # Security & Privacy > Two-Factor Authentication > Authenticator app
+    # Save the key in your .env as MFA_CODE="your_key"
+    # Copy/paste the key below and on your Robinhood app click 'Continue' on the page with the key
     totp = pyotp.TOTP("paste_key_here").now()
-    # copy the code printed and put it into your Robinhood app to link it
+    # Once you run this function/page, 
+    # copy the totp code printed and put it into your Robinhood app to link it
     print(totp)
 
 log_in()
