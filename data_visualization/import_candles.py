d230a40f7771184d5e956a66208e6d24e40e993a
sournachos <joserivera18100@gmail.com>
2024-10-05 00:04:42 -0500

    new data api

--- data_visualization/import_candles.py
+++ data_visualization/import_candles.py
@@ -6,44 +6,57 @@ import uuid
 
 import requests
 from dotenv import load_dotenv
-from jesse.modes.import_candles_mode import (
-    store_candles_list as store_candles_from_list,
-)
-from jesse.services.db import database
-from loguru import logger
+
+# from jesse.modes.import_candles_mode import (
+#     store_candles_list as store_candles_from_list,
+# )
+# from jesse.services.db import database
+
+with open("./stock_lists/mag7.json") as f:
+    tickers = json.load(f)["tickers"]
+print(tickers)
 
 load_dotenv()
 ticker: str = os.getenv("TICKER_TO_GET_HISTORICAL_CANDLES_FOR")
 start_date: str = os.getenv("HISTORICAL_CANDLES_START_DATE")
 end_date: str = os.getenv("HISTORICAL_CANDLES_END_DATE")
+api_key: str = os.getenv("API_KEY")
 
 """
 Given a ticker and a date range, it will store historical candle data
 in the local docker db instance for backtesting strategies on.
 """
+
+
 # extended_hours = "true"  # If sent as a URL parameter, returns data from 4am ET to 8pm ET
+def data_dump(ticker, start_date, end_date, api_key) -> None:
+    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={ticker}&apikey={api_key}&interval=1min&extended_hours=false"
+    candles = json.loads(requests.request("GET", url).text)
+    print(candles)
 
-url = f"https://api.marketdata.app/v1/stocks/candles/1/{ticker}?from={start_date}&to={end_date}"
 
-candles = json.loads(requests.request("GET", url).text)
+#     arr = [
+#         {
+#             "id": str(uuid.uuid4()),
+#             "exchange": "NYSE",
+#             "symbol": ticker,
+#             "timeframe": "1m",
+#             "timestamp": candles["t"][i] * 1000,
+#             "open": candles["o"][i],
+#             "close": candles["c"][i],
+#             "high": candles["h"][i],
+#             "low": candles["l"][i],
+#             "volume": candles["v"][i],
+#         }
+#         for i in range(len(candles["t"]))
+#     ]
+#     database.open_connection()
+#     store_candles_from_list(arr)
+#     database.close_connection()
 
-arr = [
-    {
-        "id": str(uuid.uuid4()),
-        "exchange": "NYSE",
-        "symbol": ticker,
-        "timeframe": "1m",
-        "timestamp": candles["t"][i] * 1000,
-        "open": candles["o"][i],
-        "close": candles["c"][i],
-        "high": candles["h"][i],
-        "low": candles["l"][i],
-        "volume": candles["v"][i],
-    }
-    for i in range(len(candles["t"]))
-]
 
-database.open_connection()
-store_candles_from_list(arr)
-database.close_connection()
-logger.info(f"Succesfully added historical candle data for {ticker}")
+if tickers:
+    for ticker in tickers:
+        data_dump(ticker, start_date, end_date, api_key)
+# else:
+#     data_dump(ticker, start_date, end_date, api_key)
